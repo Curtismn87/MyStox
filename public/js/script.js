23630702d72b4c67215b317675a69c954aab45ca
tomBeach <tbeach2k@gmail.com>
2015-08-24 21:54:51 -0400

    menu buttons, login form html *WORKING*

--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,90 @@
 $(document).ready(function(){
   console.log("document.ready");
 
+  // ======= Elements =======
+  var Elements = {};
+
+  Elements = function() {
+    console.log("Elements");
+    this.menu0 = $(".menuLink").eq(0);
+    this.menu1 = $(".menuLink").eq(1);
+    this.menu2 = $(".menuLink").eq(2);
+    this.menu3 = $(".menuLink").eq(3);
+    this.menu4 = $(".menuLink").eq(4);
+    this.menu5 = $(".menuLink").eq(5);
+    // this.menu6 = $(".menuLink").eq(6);
+    this.initEventListeners();
+  }
+
+  Elements.prototype.initEventListeners = function() {
+    console.log("initEventListeners");
+    self = this;
+
+    $(this.menu0).on("click", function(){
+      console.log("click0");
+      self.loginForm();
+    });
+    $(this.menu1).on("click", function(){
+      console.log("click1");
+    });
+    $(this.menu2).on("click", function(){
+      console.log("click2");
+    });
+    $(this.menu3).on("click", function(){
+      console.log("click3");
+    });
+    $(this.menu4).on("click", function(){
+      console.log("click4");
+    });
+    $(this.menu5).on("click", function(){
+      console.log("click5");
+    });
+    $(this.menu6).on("click", function(){
+      console.log("click6");
+    });
+  }
+
+  Elements.prototype.loginForm = function(stockJson) {
+      console.log("loginForm");
+
+      $(".contents").html("");
+      this.$el = $("<div class='loginForm'></div>");
+      console.log("this.$el: " + this.$el);
+
+      var htmlString = $("<div>");
+      htmlString.append("<input type='text' name='username'> username<br>");
+      htmlString.append("<input type='text' name='password'> password<br>");
+      htmlString.append("<input type='button' id='loginBtn' value='enter'><br>");
+      htmlString.append("</div>");
+
+      this.$el.html(htmlString);
+      $(".contents").append(this.$el);
+      $(".contents").css("margin-top", "100px");
+      this.loginBtn = $("#loginBtn");
+      $(this.loginBtn).on("click", function(){
+        console.log("loginBtn");
+      })
+
+  }
+
+
+  new Elements(function() {
+    console.log("Elements");
+
+  });
+
+  // ======= Markit =======
   var Markit = {};
 
-  Markit.QuoteService = function(sSymbol, fCallback) {
-      this.symbol = sSymbol;
-      this.fCallback = fCallback;
-      this.DATA_SRC = "http://dev.markitondemand.com/Api/v2/Quote/jsonp";
-      this.makeRequest();
+  Markit = function(sSymbol, fCallback) {
+    console.log("Markit");
+    this.symbol = sSymbol;
+    this.fCallback = fCallback;
+    this.DATA_SRC = "http://dev.markitondemand.com/Api/v2/Quote/jsonp";
+    this.makeRequest();
   };
 
-  Markit.QuoteService.prototype.makeRequest = function() {
+  Markit.prototype.makeRequest = function() {
     console.log("makeRequest");
       //Abort any open requests
       if (this.xhr) { this.xhr.abort(); }
@@ -26,7 +100,7 @@ $(document).ready(function(){
       });
   };
 
-  Markit.QuoteService.prototype.displayStock = function(stockJson) {
+  Markit.prototype.displayStock = function(stockJson) {
       console.log("displayStock");
 
       this.$el = $("<div class='stock'></div>");
@@ -40,32 +114,27 @@ $(document).ready(function(){
       htmlString.append("</div>");
 
       this.$el.html(htmlString);
-      $(".stockData").append(this.$el);
+      $(".contents").append(this.$el);
 
   }
 
-  Markit.QuoteService.prototype.handleSuccess = function(stockJson) {
+  Markit.prototype.handleSuccess = function(stockJson) {
     console.log("handleSuccess");
       this.fCallback(stockJson);
   };
 
-  Markit.QuoteService.prototype.handleError = function(stockJson) {
+  Markit.prototype.handleError = function(stockJson) {
       console.error(stockJson);
   };
 
 
-  new Markit.QuoteService("AAPL", function(stockJson) {
-    console.log("Markit.QuoteService");
+  new Markit("AAPL", function(stockJson) {
+    console.log("Markit");
 
-      //Catch errors
       if (!stockJson || stockJson.Message){
           console.error("Error: ", stockJson.Message);
           return;
       }
-
-      //If all goes well, your quote will be here.
-      // console.log(stockJson);
-      // console.log(stockJson.Name);
       this.displayStock(stockJson);
 
   });
