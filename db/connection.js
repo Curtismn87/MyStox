42b0d17f597922fe5e9235b0ecd910601e2737c2
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-08-25 12:11:40 -0400

    added join table

--- db/connection.js
+++ db/connection.js
@@ -2,17 +2,22 @@
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize("postgres:///mystox_db");
 
-var User = sequelize.import("../models/user");
-var Stock = sequelize.import("../models/stock");
+var User = sequelize.import("../app/models/user");
+var Stock = sequelize.import("../app/models/stock");
+var Portfolio = sequelize.import("../app/models/portfolio");
 
-Stock.hasMany(User);
-User.hasMany(Stock);
+//Stock.belings(User);
+//User.hasMany(Stock);
+
+Stock.belongsToMany(User, {through: 'portfolio'});
+User.belongsToMany(Stock, {through: 'portfolio'});
 
 module.exports = {
   sql: Sequelize,
   db: sequelize,
   models: {
     User: User,
-    Stock: Stock
+    Stock: Stock,
+    Portfolio: Portfolio
   }
 };
