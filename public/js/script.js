689132915617828194ef857614d4e5fd358b9a44
tomBeach <tbeach2k@gmail.com>
2015-08-25 10:09:24 -0400

    multiple stocks request working ***COOL***

--- public/js/script.js
+++ public/js/script.js
@@ -108,6 +108,10 @@ $(document).ready(function(){
             console.error("Error: ", stockJson.Message);
             return;
         }
+        console.log("stockJson.results: " + stockJson.results);
+        console.log("stockJson.results[0]: " + stockJson.results[0]);
+        console.log("stockJson.results[0].close: " + stockJson.results[0].close);
+        console.log("stockJson.results[0].lastPrice: " + stockJson.results[0].lastPrice);
         this.displayStock(stockJson);
 
     });
@@ -117,8 +121,8 @@ $(document).ready(function(){
       for (var i = 0; i < sSymbolArray.length; i++) {
         this.symbol = sSymbolArray[i];
         this.fCallback = fCallback;   // fCallback = error or process/display json
-        this.dataSource = "http://dev.markitondemand.com/Api/v2/Quote/jsonp";
-        // this.dataSource = "http://marketdata.websol.barchart.com/getQuote.json?key=5c566d2e239b7f0d6f2c73f38a767326&symbols=GOOGL";
+        // this.dataSource = "http://dev.markitondemand.com/Api/v2/Quote/jsonp";
+        this.dataSource = "http://marketdata.websol.barchart.com/getQuote.jsonp?key=5c566d2e239b7f0d6f2c73f38a767326&symbols=GOOGL,AAPL";
       }
     };
 
@@ -147,20 +151,40 @@ $(document).ready(function(){
         container.css("margin-top", 0);
         this.$el = $("<div class='stock'></div>");
 
-        var htmlString = $("<table><tr><th>Name</th><th>Symbol</th><th>Low</th><th>High</th><th>LastPrice</th></tr>");
-        htmlString.append("<tr><td><h3>" + stockJson.Name + "</h3></td>");
-        htmlString.append("<td>" + stockJson.Symbol + "</td>");
-        htmlString.append("<td>" + stockJson.Low + "</td>");
-        htmlString.append("<td>" + stockJson.High + "</td>");
-        htmlString.append("<td>" + stockJson.LastPrice + "</td>");
-        htmlString.append("</tr></table>");
-
+        var htmlString = "<table><tr><th>Name</th><th>Symbol</th><th>Low</th><th>High</th><th>LastPrice</th></tr>" +
+        // "<tr><td><h3>" + stockJson.Name + "</h3></td>" +
+        // "<td>" + stockJson.Symbol + "</td>" +
+        // "<td>" + stockJson.Low + "</td>" +
+        // "<td>" + stockJson.High + "</td>" +
+        // "<td>" + stockJson.LastPrice + "</td>" +
+        "<tr><td><h3>" + stockJson.results[0].name + "</h3></td>" +
+        "<td>" + stockJson.results[0].symbol + "</td>" +
+        "<td>" + stockJson.results[0].low + "</td>" +
+        "<td>" + stockJson.results[0].high + "</td>" +
+        "<td>" + stockJson.results[0].lastPrice + "</td></tr>" +
+        "<tr><td><h3>" + stockJson.results[1].name + "</h3></td>" +
+        "<td>" + stockJson.results[1].symbol + "</td>" +
+        "<td>" + stockJson.results[1].low + "</td>" +
+        "<td>" + stockJson.results[1].high + "</td>" +
+        "<td>" + stockJson.results[1].lastPrice + "</td>" +
+        "</tr></table>";
         this.$el.html(htmlString);
+
+        // var htmlString = $("<table><tr><th>Name</th><th>Symbol</th><th>Low</th><th>High</th><th>LastPrice</th></tr>");
+        // htmlString.append("<tr><td><h3>" + stockJson.Name + "</h3></td>");
+        // htmlString.append("<td>" + stockJson.Symbol + "</td>");
+        // htmlString.append("<td>" + stockJson.Low + "</td>");
+        // htmlString.append("<td>" + stockJson.High + "</td>");
+        // htmlString.append("<td>" + stockJson.LastPrice + "</td>");
+        // htmlString.append("</tr></table>");
+
+        // this.$el.html(htmlString);
         $(".contents").append(this.$el);
     }
 
     StockData.prototype.handleSuccess = function(stockJson) {
       console.log("handleSuccess");
+      console.log("stockJson: " + stockJson);
         this.fCallback(stockJson);
     };
 
