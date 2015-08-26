a8b4306d346d929b1b18283b4c9a7a1b6cc56fe6
tomBeach <tbeach2k@gmail.com>
2015-08-26 16:16:03 -0400

    crude d3 graph appearing from the fog

--- public/js/script.js
+++ public/js/script.js
@@ -148,13 +148,8 @@ $(document).ready(function(){
         this.$el = $("<div class='stock'></div>");
 
         var displayObject = this.stockDataObject;
-        console.log("  displayObject: " + displayObject)
-        console.log("  displayObject.name: " + displayObject.name)
-        console.log("  displayObject.stockObjectsArray[0]: " + displayObject.stockObjectsArray[0])
-        console.log("  displayObject.stockObjectsArray.length: " + displayObject.stockObjectsArray.length)
 
         var stockCount = displayObject.stockObjectsArray.length;
-        console.log("stockCount: " + stockCount);
         var htmlString = "<table><tr><th>Name</th><th>Symbol</th><th>Low</th><th>High</th><th>LastPrice</th><th>change</th></tr>";
 
         for (var i = 0; i < displayObject.stockObjectsArray.length; i++) {
@@ -342,8 +337,6 @@ $(document).ready(function(){
         //     this.ajaxRequest.abort();
         // }
         this.ajaxRequest = $.ajax({
-            // url: this.dataSource,                       // markitondemand API
-            // data: { symbol: symbolString },             // markitondemand API
             url: this.dataSource,     // barchart API
             type: 'GET',
             dataType: 'jsonp',
@@ -357,17 +350,245 @@ $(document).ready(function(){
 
     // ======= ======= ======= getAjaxGroupData ======= ======= =======
     StockData.prototype.extractGraphData = function(jsonData) {
-        console.log("getAjaxHistoryData");
+        console.log("extractGraphData");
         console.dir(jsonData);
 
         var closeValuesArray = [];
-        for (i = 0; i < jsonData.results.length; i++) {
-            nextClose = jsonData.results[i].close;
+        // for (i = 0; i < jsonData.results.length; i++) {
+        var emptyObject = { "x":null, "y":null }
+        for (i = 0; i < 10; i++) {
             nextDate = jsonData.results[i].tradingDay;
-            nextDataPoint = [nextDate, nextClose];
-            closeValuesArray.push(nextDataPoint);
+            nextClose = jsonData.results[i].close;
+            emptyObject.x = nextDate;
+            emptyObject.y = nextClose;
+            closeValuesArray.push(emptyObject);
         }
 
+        console.dir(closeValuesArray[0]);
+        console.log("closeValuesArray[0]: " + closeValuesArray[0]);
 
+        $(".contents").html("");
+
+        //The data for our line
+        // var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
+        //                  { "x": 40,  "y": 10}, { "x": 60,  "y": 40},
+        //                  { "x": 80,  "y": 5},  { "x": 100, "y": 60}];
+
+        // var lineData = [ [1, 1], [3, 3], [5, 2],  [6, 4],  [7, 3],  [9, 9] ];
+
+        var parseDate = d3.time.format("%Y-%c-%d").parse;
+
+        //This is the accessor function we talked about above
+        var lineFunction = d3.svg.line()
+            // .x(function(d) { return d.x; })
+            // .y(function(d) { return d.y; })
+            .x(function(d) {
+                x = parseDate(d.x);
+                return x;
+            })
+            .y(function(d) { return d.y; })
+            .interpolate("linear");
+
+       //The SVG Container
+       var svgContainer = d3.select(".contents").append("svg")
+            .attr("width", 200)
+            .attr("height", 200)
+            .attr("fill", "red");
+
+       //The line SVG Path we draw
+       var lineGraph = svgContainer.append("path")
+                                //    .attr("d", lineFunction(lineData))
+                                   .attr("d", lineFunction(closeValuesArray))
+                                   .attr("stroke", "blue")
+                                   .attr("stroke-width", 2)
+                                   .attr("fill", "none");
+
+
+        // console.log("closeValuesArray.length: " + closeValuesArray.length);
+        // console.log("closeValuesArray[0]: " + closeValuesArray[0]);
+        // console.log("closeValuesArray[0][0]: " + closeValuesArray[0][0]);
+        // console.log("closeValuesArray[0][1]: " + closeValuesArray[0][1]);
+
+        // ======= ======= ======= d3 processing ======= ======= =======
+        // ======= ======= ======= d3 processing ======= ======= =======
+        // ======= ======= ======= d3 processing ======= ======= =======
+
+        // var closeValuesArray = [["01/10/13", 99.61707], ["01/11/13", 99.61707], ["01/12/13", 99.61707], ["01/13/13", 99.61707], ["01/14/13", 99.61707]];
+        //
+        // // == positioning data for elements
+        // var margin = {top: 30, right: 20, bottom: 30, left: 50},
+        // width = 600 - margin.left - margin.right,
+        // height = 270 - margin.top - margin.bottom;
+        //
+        // var parseDate = d3.time.format("%Y-%c-%d").parse;
+        //
+        // var x = d3.time.scale().range([0, width]);
+        // var y = d3.scale.linear().range([height, 0]);
+        //
+        // // == create svg axes
+        // var xAxis = d3.svg.axis().scale(x)
+        //     .orient("bottom").ticks(5);
+        //
+        // var yAxis = d3.svg.axis().scale(y)
+        //     .orient("left").ticks(5);
+        //
+        // // == path generator function for lines
+        // var lineFunction = d3.svg.line()
+        //     // == accessor functions which the path generator uses to produce path data
+        //     .x(function(d) { return x(d.date); })
+        //     .y(function(d) { return y(d.close); })
+        //     .interpolate("linear");
+        //
+        // // == svg container
+        // var svgContainer = d3.select(".contents")
+        //     .append("svg")
+        //     .attr("width", 500)
+        //     .attr("height", 200);
+        //
+        // // == svg path
+        // var lineGraph = svgContainer.append("path")
+        //     .attr("d", lineFunction(closeValuesArray))
+        //     .attr("stroke", "blue")
+        //     .attr("stroke-width", 2)
+        //     .attr("fill", "none");
+
+        // var svg = d3.select(".contents")
+        //     .append("svg")
+        //         .attr("width", width + margin.left + margin.right)
+        //         .attr("height", height + margin.top + margin.bottom)
+        //     .append("g")
+        //         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //
+        //     console.dir(svg);
+
+            // svg.data(closeValueArray).enter().data(function(d){
+            //   console.log(d);
+            // })
+
+            // closeValuesArray.forEach(function(d) {
+            //     console.log(closeValuesArray);
+            //     d.date = parseDate(d.date);
+            //     d.close = +d.close;
+            //     console.log("d.date: " + d.date);
+            //
+            //     // Scale the range of the data
+            //     x.domain(d3.extent(closeValuesArray, function(d) { return d.date; }));
+            //     y.domain([0, d3.max(closeValuesArray, function(d) { return d.close; })]);
+            //
+            //     svg.append("path")      // Add the valueline path.
+            //         .attr("d", valueline(closeValuesArray));
+            //
+            //     svg.append("g")         // Add the X Axis
+            //         .attr("class", "x axis")
+            //         .attr("transform", "translate(0," + height + ")")
+            //         .call(xAxis);
+            //
+            //     svg.append("g")         // Add the Y Axis
+            //         .attr("class", "y axis")
+            //         .call(yAxis);
+            //
+            // });
+
+        // var margin = {top: 80, right: 80, bottom: 80, left: 80},
+        //     width = 960 - margin.left - margin.right,
+        //     height = 500 - margin.top - margin.bottom;
+        //
+        // var parse = d3.time.format("%b %Y").parse;
+        //
+        // // Scales and axes. Note the inverted domain for the y-scale: bigger is up!
+        // var x = d3.time.scale().range([0, width]),
+        //     y = d3.scale.linear().range([height, 0]),
+        //     xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true),
+        //     yAxis = d3.svg.axis().scale(y).ticks(4).orient("right");
+        //
+        // // An area generator, for the light fill.
+        // var area = d3.svg.area()
+        //     .interpolate("monotone")
+        //     .x(function(d) { return x(d.date); })
+        //     .y0(height)
+        //     .y1(function(d) { return y(d.price); });
+        //
+        // // A line generator, for the dark stroke.
+        // var line = d3.svg.line()
+        //     .interpolate("monotone")
+        //     .x(function(d) { return x(d.date); })
+        //     .y(function(d) { return y(d.price); });
+        //
+        // d3.csv("readme.csv", type, function(error, data) {
+        //
+        //   // Filter to one symbol; the S&P 500.
+        //   var values = data.filter(function(d) {
+        //     return d.symbol == "S&P 500";
+        //   });
+        //
+        //   // Compute the minimum and maximum date, and the maximum price.
+        //   x.domain([values[0].date, values[values.length - 1].date]);
+        //   y.domain([0, d3.max(values, function(d) { return d.price; })]).nice();
+        //
+        //   // Add an SVG element with the desired dimensions and margin.
+        //   var svg = d3.select("body").append("svg")
+        //       .attr("width", width + margin.left + margin.right)
+        //       .attr("height", height + margin.top + margin.bottom)
+        //     .append("g")
+        //       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        //       .on("click", click);
+        //
+        //   // Add the clip path.
+        //   svg.append("clipPath")
+        //       .attr("id", "clip")
+        //     .append("rect")
+        //       .attr("width", width)
+        //       .attr("height", height);
+        //
+        //   // Add the area path.
+        //   svg.append("path")
+        //       .attr("class", "area")
+        //       .attr("clip-path", "url(#clip)")
+        //       .attr("d", area(values));
+        //
+        //   // Add the x-axis.
+        //   svg.append("g")
+        //       .attr("class", "x axis")
+        //       .attr("transform", "translate(0," + height + ")")
+        //       .call(xAxis);
+        //
+        //   // Add the y-axis.
+        //   svg.append("g")
+        //       .attr("class", "y axis")
+        //       .attr("transform", "translate(" + width + ",0)")
+        //       .call(yAxis);
+        //
+        //   // Add the line path.
+        //   svg.append("path")
+        //       .attr("class", "line")
+        //       .attr("clip-path", "url(#clip)")
+        //       .attr("d", line(values));
+        //
+        //   // Add a small label for the symbol name.
+        //   svg.append("text")
+        //       .attr("x", width - 6)
+        //       .attr("y", height - 6)
+        //       .style("text-anchor", "end")
+        //       .text(values[0].symbol);
+        //
+        //   // On click, update the x-axis.
+        //   function click() {
+        //     var n = values.length - 1,
+        //         i = Math.floor(Math.random() * n / 2),
+        //         j = i + Math.floor(Math.random() * n / 2) + 1;
+        //     x.domain([values[i].date, values[j].date]);
+        //     var t = svg.transition().duration(750);
+        //     t.select(".x.axis").call(xAxis);
+        //     t.select(".area").attr("d", area(values));
+        //     t.select(".line").attr("d", line(values));
+        //   }
+        // });
+        //
+        // // Parse dates and numbers. We assume values are sorted by date.
+        // function type(d) {
+        //   d.date = parse(d.date);
+        //   d.price = +d.price;
+        //   return d;
+        // }
     }
 });
