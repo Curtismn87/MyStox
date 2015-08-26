976024d564c94af7e58378a71512a2b8e371ac58
tomBeach <tbeach2k@gmail.com>
2015-08-26 08:16:35 -0400

    temp local API ajax done (not tested) menu updates partially ok

--- public/js/script.js
+++ public/js/script.js
@@ -2,56 +2,99 @@
 $(document).ready(function(){
     console.log("document.ready");
 
+    // == make new StockData instance
+    function makeStockDataObject(whichGroup) {
+        console.log("makeStockDataObject");
+        var groupDataObject = new StockData(whichGroup);    // constructor
+        groupDataObject.name = whichGroup;                  // store group name on object
+        groupDataObject.displayObject = this;               // store display object
+        console.log("  this.name: " + groupDataObject.name);
+        return groupDataObject;
+    }
+
     // ======= ======= ======= DisplayObject ======= ======= =======
+    // ======= ======= ======= DisplayObject ======= ======= =======
+    // ======= ======= ======= DisplayObject ======= ======= =======
+
     function Display(whichDisplay) {
         console.log('Display');
         this.name = whichDisplay;
+        this.menuArray = null;
     }
     var displayObject = new Display("display1");		// constructor
 
     // ======= ======= ======= initEventListeners ======= ======= =======
     Display.prototype.initMenuDivs = function() {
         console.log("initMenuDivs");
-        this.menu0 = $(".menuLink").eq(0);
-        this.menu1 = $(".menuLink").eq(1);
-        this.menu2 = $(".menuLink").eq(2);
-        this.menu3 = $(".menuLink").eq(3);
-        this.menu4 = $(".menuLink").eq(4);
-        this.menu5 = $(".menuLink").eq(5);
+        console.log('  this.name: ' + this.name);
+
+        var menuArray = [];
+        var menuLength = $(".menuLink").length;
+        for (i = 0; i < menuLength; i++) {
+            menuArray.push($(".menuLink").eq(i));
+        }
+        this.menuArray = menuArray;
     }
 
     // ======= ======= ======= initEventListeners ======= ======= =======
     Display.prototype.initEventListeners = function() {
         console.log("initEventListeners");
+        console.log('  this.name: ' + this.name);
+
         self = this;
-        $(this.menu0).on("click", function(){
+        $(this.menuArray[0]).on("click", function(){
             self.displayLoginForm();
         });
-        $(this.menu1).on("click", function(){
-            self.displayPortfolioGroup();
+        $(this.menuArray[1]).on("click", function(){
+            updateMenu(self.menuArray[1], self);
+            groupDataObject = makeStockDataObject("portfolio");
+            groupDataObject.getGroupData();
         });
-        $(this.menu2).on("click", function(){
-            self.displayWatchGroup();
+        $(this.menuArray[2]).on("click", function(){
+            updateMenu(self.menuArray[2], self);
+            groupDataObject = makeStockDataObject("watch");
+            groupDataObject.getGroupData();
         });
-        $(this.menu3).on("click", function(){
-            self.displaySoldGroup();
+        $(this.menuArray[3]).on("click", function(){
+            updateMenu(self.menuArray[3], self);
+            groupDataObject = makeStockDataObject("sold");
+            groupDataObject.getGroupData();
         });
-        $(this.menu4).on("click", function(){
-            self.displayIndexGroup();
+        $(this.menuArray[4]).on("click", function(){
+            updateMenu(self.menuArray[4], self);
+            groupDataObject = makeStockDataObject("index");
+            groupDataObject.getGroupData();
         });
-        $(this.menu5).on("click", function(){
+        $(this.menuArray[5]).on("click", function(){
             self.displayProfile();
         });
+
+        function updateMenu(whichMenuItem, self) {
+            console.log("updateMenu");
+            console.log('  self.name: ' + self.name);
+            for (i = 0; i < self.menuArray.length; i++) {
+                nextMenuItem = self.menuArray[i];
+                console.log("  nextMenuItem: " + nextMenuItem.attr('class'));
+                nextMenuItem.removeClass("thisPage");
+            }
+            whichMenuItem.addClass("thisPage");
+            for (i = 0; i < self.menuArray.length; i++) {
+                nextMenuItem = self.menuArray[i];
+                console.log("  nextMenuItem: " + nextMenuItem.attr('class'));
+            }
+        }
+
     }
 
     // ======= ======= ======= loginForm ======= ======= =======
     Display.prototype.displayLoginForm = function() {
         console.log("displayLoginForm");
+        console.log('  this.name: ' + this.name);
+        var self = this;
 
         // == init form container div
         $(".contents").html("");
         this.$el = $("<div class='loginForm'></div>");
-        console.log("this.$el: " + this.$el);
 
         // == init login form elements
         var htmlString = $("<div>");
@@ -69,31 +112,17 @@ $(document).ready(function(){
         // == activate login button
         $(this.loginBtn).on("click", function(){
             console.log("loginBtn");
-        self.displayPortfolioGroup();
+            self.updateLoginMenuItem();
+            groupDataObject = makeStockDataObject("portfolio");
+            groupDataObject.getGroupData();
         })
     }
 
-    // ======= ======= ======= menu items ======= ======= =======
-    Display.prototype.displayPortfolioGroup = function() {
-        console.log("displayPortfolioGroup");
-        getGroupData("portfolio");
-    }
-
-    Display.prototype.displayWatchGroup = function() {
-        console.log("displayWatchGroup");
-        getGroupData("watch");
-    }
-
-    Display.prototype.displaySoldGroup = function() {
-        console.log("displaySoldGroup");
-    }
-
-    Display.prototype.displayIndexGroup = function() {
-        console.log("displayIndexGroup");
-    }
-
-    Display.prototype.displayProfile = function() {
-        console.log("displayProfile");
+    // ======= ======= ======= loginForm ======= ======= =======
+    Display.prototype.updateLoginMenuItem = function() {
+        console.log("updateLoginMenuItem");
+        // this.menuArray[0].text("logout");
+        this.menuArray[0].html("<a href='#'>logout</a>");
     }
 
     displayObject.initMenuDivs();
@@ -101,29 +130,61 @@ $(document).ready(function(){
 
 
     // ======= ======= ======= StockObject ======= ======= =======
-    function StockData(whichGroup, groupArray) {
+    // ======= ======= ======= StockObject ======= ======= =======
+    // ======= ======= ======= StockObject ======= ======= =======
+
+    function StockData(whichGroup, groupArray, displayObject) {
         console.log('StockData');
-        this.name = whichGroup;
-        this.groupArray = groupArray;
-        this.dataSource = "http://marketdata.websol.barchart.com/getQuote.jsonp?key=5c566d2e239b7f0d6f2c73f38a767326&symbols=";
+        this.name = whichGroup;             // portfolio, watchlist, sold, indexes
+        this.groupArray = groupArray;       // stocks in group (returned from database)
+        this.displayObject = displayObject; // store display object for display access
+        this.jsonCallback = "???";          // callback function required for jsonp data ???
+        // this.dataSource = "http://marketdata.websol.barchart.com/getQuote.jsonp?key=5c566d2e239b7f0d6f2c73f38a767326&symbols=";
+        this.dataSource = "http://dev.markitondemand.com/Api/v2/Quote/jsonp";
+        // this.dataSource = "http://dev.markitondemand.com/Api/v2/Quote";
     }
 
-    function getGroupData(whichGroup) {
+    // ======= ======= ======= getGroupData ======= ======= =======
+    StockData.prototype.getGroupData = function() {
         console.log('getGroupData');
-        if (whichGroup = "portfolio") {
-            whichArray = ["APPL", "GOOGL", "HD"]                            // from database
-            var portfolioGroup = new StockData(whichGroup, whichArray);	    // constructor
-            symbolString = portfolioGroup.makeGroupString();
-            groupData = portfolioGroup.getAjaxGroupData();
-        } else if (whichGroup = "portfolio") {
-            whichArray = ["APPL", "GOOGL", "HD"]                           // from database
-            var watchGroup = new StockData(whichGroup, whichArray);		   // constructor
-            symbolString = watchGroup.makeGroupString();
+        console.log('  this.name: ' + this.name);
+
+        // == create url for local server API
+        if (this.name = "portfolio") {
+            urlString = "/users/:id/stocks/";
+        } else if (this.name = "watch") {
+            urlString = "/users/:id/watch/";
+        } else if (this.name = "sold") {
+            urlString = "/users/:id/sold/";
+        } else if (this.name = "index") {
+            urlString = "/users/:id/index/";
         }
+
+        // == extract stock list from database
+        // $.ajax({
+        //     url: urlString,
+        //     method: "get"
+        // }).then(function(response) {
+        //     this.groupArray = response;                     // from database
+        //     symbolStrig = this.makeGroupString();           // array => string
+        //     groupData = this.getAjaxGroupData(symbolStrig); // make ajax request
+        // }).fail(function() {
+        //     console.log("database query failed");
+        // });
+
+        // == temp stock list for development
+        // tempArray = ["APPL", "GOOGL", "HD"];
+        tempArray = ["APPL"];
+        this.groupArray = tempArray;
+        symbolString = this.makeGroupString();
+        groupData = this.getAjaxGroupData(symbolString);
+
     }
 
+    // ======= ======= ======= makeGroupString ======= ======= =======
     StockData.prototype.makeGroupString = function() {
         console.log("makeGroupString");
+        console.log("  this.name: " + this.name);
         var symbolString = '';
         for (var i = 0; i < this.groupArray.length; i++) {
             if (i == this.groupArray.length - 1) {
@@ -135,32 +196,52 @@ $(document).ready(function(){
         return symbolString;
     }
 
-    StockData.prototype.getAjaxGroupData = function(symbolString) {
+    // ======= ======= ======= getAjaxGroupData ======= ======= =======
+    StockData.prototype.getAjaxGroupData = function(symbolStrig) {
         console.log("getAjaxGroupData");
+        console.log("  this.name: " + this.name);
+        self = this;
         if (this.ajaxRequest) {
+            console.log("request aborted");
             this.ajaxRequest.abort();
         }
         this.ajaxRequest = $.ajax({
-            url: this.dataSource + symbolString,
-            // url: this.dataSource + "APPL",
+            url: this.dataSource,                       // markitondemand API
+            data: { symbol: symbolString },             // markitondemand API
+            // url: this.dataSource + symbolString,     // barchart API
             type: 'GET',
             dataType: 'jsonp',
             crossDomain: true,
-            error: this.handleError,
-            always: this.handleAlways,
-            success: this.extractGroupData,
-            // success: function(data) {console.log("data: " + data.status.message);},
-            context: this
+            error: self.handleError,
+            always: self.handleAlways,
+            success: self.extractGroupData,
+            context: self
         });
+
+        // this.ajaxRequest = $.ajax({
+        //     url: this.dataSource + symbolString,
+        //     // url: this.dataSource,
+        //     // data: { symbol: "APPL" },
+        //     type: 'GET',
+        //     dataType: 'jsonp',
+        //     crossDomain: true,
+        // })
+        // this.ajaxRequest.done(extractGroupData(data, this));
+        // this.ajaxRequest.fail(this.handleError);
     }
 
+    // ======= ======= ======= extractGroupData ======= ======= =======
     StockData.prototype.extractGroupData = function(jsonStock) {
+    // function extractGroupData(jsonStock, self) {
         console.log("extractGroupData");
-        console.log("jsonStock.code: " + jsonStock.status.code);
-        console.log("jsonStock.message: " + jsonStock.status.message);
+        console.log("  this.name: " + this.name);
+        // console.log("jsonStock.code: " + jsonStock.status.code);
+        // console.log("jsonStock.message: " + jsonStock.status.message);
         if (jsonStock.results) {
             var stockCount = jsonStock.results.length;
-            console.log("stockCount: " + stockCount);
+            console.log("  stockCount: " + stockCount);
+
+            // == store extracted data in StockData instance
             for (var i = 0; i < jsonStock.results.length; i++) {
                 nextResult = jsonStock.results[i];
                 this.symbol = nextResult.symbol;
@@ -171,12 +252,26 @@ $(document).ready(function(){
                 this.lastPrice = nextResult.lastPrice;
             }
         } else {
-            console.log("no results in this response");
+            console.log("  no results in this response");
+            this.handleError(jsonStock);
         }
     }
 
-    StockData.prototype.handleError = function() {
+    // ======= ======= ======= handleError ======= ======= =======
+    StockData.prototype.handleError = function(jsonStock) {
         console.log("handleError");
+
+        // == create string for message display
+        var msgString = "code: " + jsonStock.status.code + "<br>message: " + jsonStock.status.message
+
+        // == init message container div
+        $(".contents").html("");
+        this.$el = $("<div class='errorMessage'></div>");
+
+        // == append and position login form elements
+        this.$el.html(msgString);
+        $(".contents").append(this.$el);
+        $(".contents").css("margin-top", "100px");
     }
 
 });
