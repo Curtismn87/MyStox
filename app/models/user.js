87c3d28254e06c8790eb2eeb12919b2528de10c2
tomBeach <tbeach2k@gmail.com>
2015-08-26 21:05:45 -0400

    fixed app/connection/models path got modules/require etc.

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 // ======= ======= ======= MODEL user.js ======= ======= =======
+
 module.exports = function(sequelize, DataTypes){
   return sequelize.define("user", {
     name: DataTypes.STRING,
