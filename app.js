e1c0c219956b3150bc1901508f7d1e49ba277b8b
Matt Curtis <Curtismn87@yahoo.com>
2015-08-27 14:34:06 -0400

    app.listen change

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.use("/", usersController);
 app.use("/", stocksController);
 app.use("/", ownershipsController);
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
